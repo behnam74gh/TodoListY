fd52334d10dee81f7cf8bb9af0fbddb3ed207f9f
behnam ghazaghi <behnam74gh@yahoo.com>
2024-05-10 15:48:51 +0330

    changing status function added

--- src/App.js
+++ src/App.js
@@ -10,35 +10,34 @@ const App = () => {
     title: "",
     description: ""
   });
-  const [taskAdded, setTasksUpdated] = useState(false)
+  // const [tasksUpdated, setTasksUpdated] = useState(false);
 
   useEffect(() => {
     console.log('useEffect rendered');
-      axios
-        .get("http://localhost:3000/tasks")
-        .then((response) => {
-          console.log('=>',response);
-          if (response.status === 200) {
-            setTasks(response.data);
-            errorText?.length > 0 && setErrorText("");
-          }
-        })
-        .catch((err) => {
-          if (err.response) {
-            setErrorText(err.response.data.message);
-            setTasks([]);
-          }
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-  }, [taskAdded]);
+    axios
+      .get("http://localhost:3000/tasks")
+      .then((response) => {
+        console.log('=>',response);
+        if (response.status === 200) {
+          setTasks(response.data);
+          errorText?.length > 0 && setErrorText("");
+        }
+      })
+      .catch((err) => {
+        if (err.response) {
+          setErrorText(err.response.data.message);
+          setTasks([]);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     setLoading(true);
-    setTasksUpdated(false);
     axios
       .post("http://localhost:3000/tasks", {
         title: task.title,
@@ -51,7 +50,7 @@ const App = () => {
             title: "",
             description: ""
           });
-          setTasksUpdated(true);
+          // setTasksUpdated(true);
         }
       })
       .catch((err) => {
@@ -63,67 +62,88 @@ const App = () => {
   };
   const removeTaskHandler = (id) => {
     if (window.confirm("برای حذف این یادداشت مطئن هستید؟")) {
-      setTasksUpdated(false);
       axios
         .delete(`http://localhost:3000/tasks/${id}`)
         .then((response) => {
           if (response.status === 200) {
-            setTasksUpdated(true);
+            // setTasksUpdated(true);
           }
         })
         .catch((err) => {
           if (err) {
-            console.log(err);
+            console.log('r -> ', err);
             window.alert(err.response.statusText)
           }
         });
     }
   };
+  const changeStatusHandler = (task) => {
+    const {isDone, id} = task;
+
+    axios
+      .patch(`http://localhost:3000/tasks/${id}`, { isDone: !isDone })
+      .then((response) => {
+        if (response.status === 200) {
+          window.alert("وضعیت یادداشت تغییر کرد");
+        }
+      })
+      .catch((err) => {
+        console.log('c -> ',err);
+        if (err.response) {
+          window.alert(err.response.statusText)
+        }
+      });
+    };
+
 
   return (
     <div className='todo-container' dir='rtl'>
-        <div className="todo-wrapper">
-          <h5>یادداشت های ثبت شده</h5>
-            {loading ? (
-            <div className="w-100">
-                <span>Loading...</span>
-            </div>
-            ) : (
-            tasks.length > 0 &&
-            tasks.map((task) => (
-              <div className="task-wrapper" key={task.id}>
-                <strong className='title'>{task.title}</strong>
-                <p className='description'>{task.description}</p>
-                <div className="tasks-options-wrapper">
-                  <button type='button' className='delete-button' onClick={() => removeTaskHandler(task.id)}>حذف</button>
-                  <input
-                    type="checkbox"
-                    className="check-task-status"
-                    value={task.isDone ? true : false}
-                    checked={task.isDone ? true : false}
-                  />
-                </div>
+      <div className="form-wrapper">
+        <h5>لطفا عنوان و متن یادداشت را وارد نمایید</h5>
+        <form className="todo-form" onSubmit={submitHandler}>
+          <input type='text' placeholder='عنوان' value={task.title}
+            onChange={(e) => setTask({...task, title: e.target.value})}
+          />
+
+          <textarea value={task.description} placeholder='توضیحات' rows="5"
+            onChange={(e) => setTask({...task, description: e.target.value})}
+          ></textarea>
+          
+          <button type="submit">
+          {!loading ? "ثبت" : "Loading ..."}
+          </button>
+        </form>
+      </div>
+
+      <hr />
+
+      <div className="todo-wrapper">
+        <h5>یادداشت های ثبت شده</h5>
+          {loading ? (
+          <div className="w-100">
+              <span>Loading...</span>
+          </div>
+          ) : (
+          tasks.length > 0 &&
+          tasks.map((task) => (
+            <div className="task-wrapper" key={task.id}>
+              <strong className='title'>{task.title}</strong>
+              <p className='description'>{task.description}</p>
+              <div className="tasks-options-wrapper">
+                <button type='button' className='update-button' onClick={() => removeTaskHandler(task.id)}>ویرایش</button>
+                <button type='button' className='delete-button' onClick={() => removeTaskHandler(task.id)}>حذف</button>
+                <input
+                  type="checkbox"
+                  className="check-task-status"
+                  value={task.isDone ? true : false}
+                  checked={task.isDone ? true : false}
+                  onChange={() => changeStatusHandler(task)}
+                />
               </div>
-            ))
-            )}
-        </div>
-
-        <div className="form-wrapper">
-          <h5>لطفا عنوان و متن یادداشت را وارد نمایید</h5>
-          <form className="todo-form" onSubmit={submitHandler}>
-            <input type='text' placeholder='عنوان' value={task.title}
-              onChange={(e) => setTask({...task, title: e.target.value})}
-            />
-
-            <textarea value={task.description} placeholder='توضیحات' rows="5"
-              onChange={(e) => setTask({...task, description: e.target.value})}
-            ></textarea>
-            
-            <button type="submit">
-            {!loading ? "ثبت" : "Loading ..."}
-            </button>
-          </form>
-        </div>
+            </div>
+          ))
+          )}
+      </div>
     </div>
   )
 }
